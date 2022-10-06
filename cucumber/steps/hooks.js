c56c5c5440a8c719c6460f2c2853bd380bc6704e
Anton Rak <Anton_Rak@epam.com>
2022-10-06 16:54:00 +0300

    BeforeAllAfterAllhooks

--- cucumber/steps/hooks.js
+++ cucumber/steps/hooks.js
@@ -1,5 +1,5 @@
 const {
-  setDefaultTimeout, Before, After, Status,
+  setDefaultTimeout, Before, BeforeAll, AfterAll, Status,
 } = require('@cucumber/cucumber');
 const { remote } = require('webdriverio');
 const { po } = require('@cucumber-e2e/po2');
@@ -12,9 +12,7 @@ const { capabilities, services } = require('../../config/chrome/rp');
 
 setDefaultTimeout(timeouts.GLOBAL_TIMEOUT);
 
-Before(async () => {
-
-
+BeforeAll(async () => {
   browser = await remote({
     logLevel: 'warn',
     waitforTimeout: timeouts.WAIT_FOR_TIMEOUT,
@@ -25,8 +23,19 @@ Before(async () => {
   po.init(browser, { timeout: timeouts.DEFAULT_TIMEOUT });
   po.register(indexMap);
   storage.register(columnNamesList);
-});
 
-After(async () => {
+
+
+    await browser.url('http://localhost:8080');
+
+    const usernameInput = await po.getElement('Login Form > Login Field');
+    await usernameInput.addValue('superadmin');
+    const passwordInput = await po.getElement('Login Form >  Password Field');
+    await passwordInput.addValue('erebus');
+    const login = await po.getElement('Login Form > Login Button');
+    await login.click();
+})
+
+AfterAll(async () => {
   await browser.deleteSession();
 });
